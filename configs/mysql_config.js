3c3898c0bd99fc6299719e8fd1032ee409f212bd
SammySheu <samsheu1997@gmail.com>
2023-01-05 17:38:56 +0800

    Compare loginUser with data searched in db and response with corresponding situations.

--- configs/mysql_config.js
+++ configs/mysql_config.js
@@ -1,19 +1,19 @@
-let mysql = require('mysql2');
+const mysql = require('mysql2/promise')
 
-
-let pool = mysql.createPool({
+const pool = mysql.createPool({
   connectionLimit:4,
   host: "localhost",
   port: 3306,
   user: "localUser",
   password: "password",
-  database:"starTech_userdb"
-});
+  database:"starTech_userdb",
+})
 
-pool.getConnection((err,connection) => {
+pool.getConnection( (err, connection) => {
   if(err) throw err;
-  console.log('Database connected successfully');
-  if (connection) connection.release()
-});
+  console.log('Database connect successfully');
+  connection.release()
+})
+
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const multer = require('multer')
+const path = require('path');
+const multer = require('multer');
 const pool = require('./configs/mysql_config');
-const { type } = require('os');
+const jwt = require('jsonwebtoken');
 
 port = 3000
 
@@ -24,25 +24,40 @@ app.get('/', (req, res) => {
     res.sendFile( path.join( __dirname + '/views/frontendPage.html' ) )
 })
 
-app.get('/show', (req, res) => {
-    pool.query('SELECT * FROM user;', (err, data) => {
-        console.log(data);
-        res.send(data)
-    })
+app.get('/show', async (req, res) => {
+    const data = await pool.query('SELECT * FROM user;');
+    return data[0]
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const loginUser = req.body;
-    res.send(req.body);
+    const dataFromDB = await getUserData(loginUser.email);
+    if(dataFromDB == null) return 'Cannot find this email in MySQL';
+    else{
+        res.send(authenticateUser(loginUser, dataFromDB))
+    }
 })
+
+const getUserData = async (email) => {
+    const response = await pool.query(`SELECT * FROM user WHERE email = "${email}";`);
+    console.log(response[0][0]);
+    return response[0][0];
+}
+const authenticateUser = (loginUser, dataFromDB) => {
+    if(loginUser.password === dataFromDB.password){
+        return 'Password correct';
+    }
+    else {
+        return 'Password incorrect';
+    }
+}
 
 app.post('/register', (req, res) => {
     const registUser = req.body;
     const sql = `INSERT INTO user(email, password) VALUES ("${registUser.email}", "${registUser.password}");`;
-    pool.query(sql, (err, data) => {
-        if(err) throw err;
-        res.send('Register successfully');
-    })
+    pool.query(sql)
+        .then(res.send('Register successfully'))
+    
 })
 
 app.post('/upload',uploadMiddleWare.single('image'), (req, res) => {
